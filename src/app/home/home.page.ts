0c3d57506426b17733e02f98d8165bd53b5d52a7
Zarci <blakerile@gmail.com>
2020-05-20 18:16:20 -0400

    popup window and landmark icon update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,11 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Router, Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+
+
+
 declare var google;
 var mapOpt = { minZoom: 13, maxZoom: 0};
 var mapBoundaries = {
@@ -7,7 +13,10 @@ var mapBoundaries = {
 	south: 39.20,
 	east: -84.30,
 	west: -84.40
+
 }
+
+
 @Component({
 	selector: 'app-home',
 	templateUrl: 'home.page.html',
@@ -16,124 +25,90 @@ var mapBoundaries = {
 
 
 export class HomePage implements OnInit, AfterViewInit {
+
+
+	// Current Location Lat/Lng
 	latitude: any;
 	longitude: any;
+	
 	@ViewChild('mapElement', {static: true}) mapNativeElement: ElementRef;
-	constructor(private geolocation: Geolocation) { }
+	constructor(private geolocation: Geolocation, private router: Router) { }
 
-	ngOnInit() {
+	ngOnInit() {	
 	}
 
 	ngAfterViewInit(): void {
-		/*
-		
 
 		/* Map Parameters */
 
-
 		const map = new google.maps.Map(this.mapNativeElement.nativeElement, {
 			center: {lat: 39.251, lng: -84.342},
-			zoom: 14,
+			zoom: 14, 
 			streetViewControl: false,
 			mapTypeId: 'terrain',
 			restriction: {
 				latLngBounds: mapBoundaries,
 				strictBounds: false,
 			}
-
-
-
-
 		});
+		map.setOptions(mapOpt);
 
-		/* Map Geolocation */
+
+		/* Map Pin Icons */
+
 		const youAreHere = {
 			url: 'assets/icon/u.png',
 			scaledSize: new google.maps.Size(50,50)
-
 		};
+		const landmarkICO = {
+			url: 'assets/icon/landmark.png'
+		};
+
+
+		/* Map Geolocation */
+
 		this.geolocation.getCurrentPosition().then((resp) => {
 			this.latitude = resp.coords.latitude;
 			this.longitude = resp.coords.longitude;
 			const pos = {lat: this.latitude, lng: this.longitude};
 			map.setCenter(pos);
+
+
+			/* Current Location Marker Object */
+
 			const locationMarker = new google.maps.Marker({
 				position: pos,
 				map: map,
 				icon: youAreHere,
-				title: 'You are here!'
-				
+				title: 'You are here!'	
 			});
-			const contentOfPin1 = '<div id="content">' + '<p> Test Icon Popup </p>'
-
-			const pin1InfoWindow = new google.maps.InfoWindow({
-				content: contentOfPin1,
-				maxWidth: 400
-			});
-			marker.addListener('click', function() {
-				pin1InfoWindow.open(map,marker);
-			});
-
-
-
-
-
-
-			
-		}).catch((error) => {
-			console.log('Error getting location', error);
 		});
 
+		/* Marker with link to page creation */
 
 
-		/* Map Parameters */
+		function addLinikedMarker (lat, lng, icon, popupString: string, name: string) {
 
-
-		map.setOptions(mapOpt);
-
-
-		/* Map Pin Creation */
-
-
-		var iconBase =
-		'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
-
-		var icons = {
-			parking: {
-				icon: iconBase + 'parking_lot_maps.png'
-			},
-			library: {
-				icon: iconBase + 'library_maps.png'
-			},
-			info: {
-				icon: iconBase + 'info-i_maps.png'
-			},
-			home: {
-				icon: youAreHere
-			}
-		};
-
-		var features = [
-		{
-			position: new google.maps.LatLng(39.223251, -84.355784),
-			type: 'home'
-		}];
-
-		// Create markers.
-		for (var i = 0; i < features.length; i++) {
 			var marker = new google.maps.Marker({
-				position: features[i].position,
-				icon: icons[features[i].type].icon,
+				position: new google.maps.LatLng(lat,lng),
+				icon: icon,
 				map: map
 			});
-		};
+			var infoWindow = new google.maps.InfoWindow({
+				content: popupString
+			});
+			marker.addListener('click', function () {
+				infoWindow.open(map, marker);
+			});	
+		}
+		/* Linked Marker Calls */
 
 
 
+		var summitPark = '<p> <b>test</b> window </p>'
+		addLinikedMarker (39.250511, -84.387455, landmarkICO, summitPark, 'summit park');
 
 
-
-
+		
 	}
-
 }
